760aa05240d14b5ff6a19b1c4fa804696af5f236
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2025-10-03 19:35:26 +0530

    Implements removal of duplicates in sorted array
    Adds a solution for removing duplicate elements from a sorted integer array. It utilizes an efficient two-pointer approach to modify the array in-place, retaining only unique elements.

---
+++ problem_basic/stl/03_remove-duplicate-from-sorted-array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+
+vector<int> removeDuplicates(vector<int> nums){
+
+    int i = 0;
+
+    for (int j = 1; j < nums.size();j++){
+        if (nums[j] != nums[i]){
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    nums.resize(i + 1);
+
+    return nums;
+}
+
+int main() {
+    vector<int> arr = {0,0,1,1,1,2,2,3,3,4};
+    vector<int> res = removeDuplicates(arr);
+
+    for (int x : res) cout << x << " ";
+    cout << endl;
+}

7b3299bea7c7e8c1b89de32d80003ca45486d549
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-01-02 13:52:43 +0530

    feat: Initialize cron jobs via `instrumentation.ts` and add error handling to the report cron job.

--- nextjs-16/app/server/cron/reports.ts
+++ nextjs-16/app/server/cron/reports.ts
@@ -2,17 +2,23 @@ import cron from "node-cron";
 
 export async function registerReportCron() {
   cron.schedule("0 */20 * * * *", async () => {
-    const response = await fetch("https://chat.yagyaraj.com/api/room/create", {
-      headers: {
-        accept: "*/*",
-        "content-type": "application/json",
-      },
-      body: '{"username":"anon-lion-gxn"}',
-      method: "POST",
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+    try {
+      const response = await fetch(
+        "https://chat.yagyaraj.com/api/room/create",
+        {
+          headers: {
+            accept: "*/*",
+            "content-type": "application/json",
+          },
+          body: '{"username":"anon-lion-gxn"}',
+          method: "POST",
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error(error);
     }
   });
 }
--- nextjs-16/app/server/index.ts
+++ nextjs-16/app/server/index.ts
@@ -1,7 +1,9 @@
 import { registerCleanupCron } from "./cron/cleanup";
 import { registerReportCron } from "./cron/reports";
 
-registerCleanupCron();
-registerReportCron();
+export function initCronJobs() {
+  registerCleanupCron();
+  registerReportCron();
 
-console.log("server started");
+  console.log("server started");
+}

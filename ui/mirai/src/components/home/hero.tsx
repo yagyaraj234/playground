b98622f4256a5a1c7d06d3312d5426224bee9d2d
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-02-27 22:53:00 +0530

    feat(home): add hero section with image and update header
    Add Hero and Info components to the main page to enhance the landing page content.
    Update header button styling for better visual consistency.

---
+++ ui/mirai/src/components/home/hero.tsx
@@ -0,0 +1,25 @@
+import { IconArrowNarrowRight } from '@tabler/icons-react'
+
+export default function Hero() {
+  return (
+    <main className="p-4 lg:px-8 select-none">
+      <h1 className="text-5xl font-bold text-white mt-12 leading-relaxed">
+        Your models. Every Applce device.
+      </h1>
+      <h2 className="text-5xl font-semibold text-text">
+        The fastest inference engine for Apple Silicon{' '}
+      </h2>
+
+      <button className="bg-white text-zinc-950 py-3 px-6 rounded-lg my-16 cursor-pointer flex gap-6 items-center">
+        Talk to use
+        <IconArrowNarrowRight size={20} className="text-black" />
+      </button>
+
+      <div className="border border-zinc-800 p-1.5">
+        <img src="./images/hero.png" height={'100%'} width={'100%'} />
+          </div>
+          
+          
+    </main>
+  )
+}

5ede73106013543eaf3afec90be9f992e52d6282
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-03-08 10:45:57 +0530

    feat(ui): add benchmarks section with model and chip selection
    Introduce a new "Deliverables" component to the home page that displays benchmarks for different models and Apple chips. The component includes dropdown menus for selecting models and chips, allowing users to view performance metrics. Additionally, adjust typography spacing in existing components for better visual consistency.

---
+++ ui/mirai/src/components/menu.tsx
@@ -0,0 +1,41 @@
+import { Menu } from '@base-ui/react/menu'
+import { IconChevronDown } from '@tabler/icons-react'
+
+type DropDownMenu<T> = {
+  onSelect: (item: T) => void
+  data: T[]
+  selectedItem: string | number
+  renderItem: (item: T) => React.ReactElement
+}
+
+export default function DropDownMenu<T>(props: DropDownMenu<T>) {
+  return (
+    <Menu.Root>
+      <Menu.Trigger
+        className={` flex gap-4 items-center border font-inter text-zinc-200 border-zinc-800  py-2`}
+      >
+        <span className="px-4 flex items-center gap-4">
+          {props.selectedItem} <IconChevronDown size={20} />
+        </span>
+      </Menu.Trigger>
+      <Menu.Portal className={`bg-zinc-800`}>
+        <Menu.Backdrop />
+        <Menu.Positioner className={``} sideOffset={8} side="bottom">
+          <Menu.Popup className={`bg-zinc-900 border border-zinc-700`}>
+            {props.data.map((item, idx) => {
+              return (
+                <Menu.Item
+                  className={`py-2 px-3 items-center hover:bg-zinc-800`}
+                  key={idx}
+                  onSelect={() => props.onSelect(item)}
+                >
+                  {props.renderItem(item)}
+                </Menu.Item>
+              )
+            })}
+          </Menu.Popup>
+        </Menu.Positioner>
+      </Menu.Portal>
+    </Menu.Root>
+  )
+}

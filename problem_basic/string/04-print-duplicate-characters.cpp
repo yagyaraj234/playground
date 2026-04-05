c067753828bfbc8f5e06f611513d15ac495e6838
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-04-05 16:13:43 +0530

    solved problem: 04-print-duplicate-characters

--- problem_basic/string/04-print-duplicate-characters.cpp
+++ problem_basic/string/04-print-duplicate-characters.cpp
@@ -1,10 +1,29 @@
-// Problem 04: Print Duplicate Characters
-// Description: Print duplicate characters from a given string.
-
 #include <iostream>
 #include <string>
+using namespace std;
+
+void printDuplicate(string str) {
+
+    int arr[26] = {0};
+
+    // Count frequency
+    for (int i = 0; i < str.length(); i++) {
+        int index = str[i] - 'A';
+        arr[index]++;
+    }
+
+    // Print duplicates
+    for (int i = 0; i < 26; i++) {
+        if (arr[i] > 1) {
+            cout << "Duplicate character: "
+                 << char('A' + i)
+                 << endl;
+        }
+    }
+}
 
 int main() {
-    // TODO: Implement solution.
+    string str = "ABCBDA";
+    printDuplicate(str);
     return 0;
 }

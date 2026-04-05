3096d0b6c276fceae08a04a6c9e1d9fc74d72020
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-04-05 16:19:12 +0530

    solved problem: 05-count-character-occurrences-without-loop

--- problem_basic/string/05-count-character-occurrences-without-loop.cpp
+++ problem_basic/string/05-count-character-occurrences-without-loop.cpp
@@ -1,10 +1,33 @@
-// Problem 05: Count Character Occurrences Without Loop
-// Description: Count occurrences of a character without using loops.
-
 #include <iostream>
 #include <string>
+using namespace std;
+
+void printDuplicate(string str) {
+
+    int arr[26] = {0};
+
+    // Count frequency
+    for (int i = 0; i < str.length(); i++) {
+        char ch = str[i];
+        if (ch >= 'a' && ch <= 'z') {
+                ch = ch - 32;   // correct offset
+            }
+        int index = ch - 'A';
+        arr[index]++;
+    }
+
+    // Print duplicates
+    for (int i = 0; i < 26; i++) {
+        if (arr[i] > 1) {
+            cout << "Duplicate character: "
+                 << char('A' + i) <<' '<< arr[i]
+                 << endl;
+        }
+    }
+}
 
 int main() {
-    // TODO: Implement solution.
+    string str = "AaBCEWREWSFDSAQWFABDA";
+    printDuplicate(str);
     return 0;
 }

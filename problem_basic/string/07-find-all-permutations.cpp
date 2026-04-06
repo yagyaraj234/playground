0f839c4aa2d13077d28cde4e4c605bf5685f9a30
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-04-06 09:56:05 +0530

    solved 5 more problems on string

--- problem_basic/string/07-find-all-permutations.cpp
+++ problem_basic/string/07-find-all-permutations.cpp
@@ -3,8 +3,50 @@
 
 #include <iostream>
 #include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+
+
+void recurPermutate(int idx, string &s, vector<string> &res){
+
+    if(idx == s.size()){
+
+        res.push_back(s);
+        return;
+
+    }
+
+    for(int i=idx;i<s.size();i++){
+        swap(s[i],s[idx]);
+        recurPermutate(idx+1, s,res);
+        swap(s[i],s[idx]);
+    }
+
+
+}
+
+void PrintPermutations(std::string str){
+
+    std::vector<std::string> v;
+    int len =str.length()-1;
+
+    recurPermutate(0,str,v);
+
+
+    for (auto it = v.begin(); it != v.end(); it++) {
+        std::cout << *it << " "; // Dereference to access value
+    }
+
+}
 
 int main() {
-    // TODO: Implement solution.
+
+    std::string str ="ABC";
+
+    PrintPermutations(str);
+
     return 0;
 }
--- problem_basic/string/10-balanced-brackets-without-stack.cpp
+++ problem_basic/string/10-balanced-brackets-without-stack.cpp
@@ -3,8 +3,42 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
 
+bool isBalanced(string str){
+
+    int i=0;
+    int j=str.size()-1;
+
+    while(i<j){
+
+        char left =str[i];
+        char right =str[j];
+
+        if(left=='{' && right=='}'){
+            i++;
+            j--;
+            continue;
+        }else if(left=='[' && right==']'){
+            i++;
+            j--;
+            continue;
+        }else if(left=='(' && right==')'){
+            i++;
+            j--;
+            continue;
+        }else{
+            return false;
+        }
+
+    }
+
+    return true;
+
+}
 int main() {
     // TODO: Implement solution.
+    cout<<"{}[]{ : "<< isBalanced("{}")<<endl;
+    cout<<"{}[{}]{} : "<< isBalanced("{}[{}]{}")<<endl;
     return 0;
 }

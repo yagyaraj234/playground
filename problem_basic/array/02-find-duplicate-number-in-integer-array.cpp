efc11af99e43f87ced3442211ae464ef30b2ea5f
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-04-07 08:12:51 +0530

    problem solved: find duplicates

--- problem_basic/array/02-find-duplicate-number-in-integer-array.cpp
+++ problem_basic/array/02-find-duplicate-number-in-integer-array.cpp
@@ -4,5 +4,48 @@ using namespace std;
 // Problem: How to find duplicate number on Integer array in cpp?
 
 int main() {
+
+    int arr[50]={0};
+    int occ[50]={0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout<<"size: "<<n<<endl<<endl;
+    for(int it=0;it<n;it++){
+        int random_num=(rand() % n) + 1;
+        arr[it]=random_num;
+    }
+
+    sort(arr,arr+n);
+    cout<<"Printing number "<<endl<<endl;
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl<<endl;
+
+    //  store the occurunce  of repeating numbers
+    //
+    for(int i=0;i<n;i++){
+        occ[arr[i]-1]++;
+    }
+
+
+    cout<<"printing occ"<<endl<<endl;
+
+    for(int i=0;i<n;i++){
+        cout<<occ[i]<<" ";
+    }
+    cout<<endl<<endl;
+    cout<<"Printing duplicate numbers "<<endl;
+
+    for(int i=0;i<n;i++){
+        if(occ[i]> 1){
+            cout<<i+1<<" ";
+        }
+    }
+
+
+
+
+
     return 0;
 }

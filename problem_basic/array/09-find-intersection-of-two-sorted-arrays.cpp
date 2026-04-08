2eda0d76444982a623af05d9c2f8768c890e3c7f
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-04-08 10:27:37 +0530

    problem solved: insectetion of two sorted array

--- problem_basic/array/09-find-intersection-of-two-sorted-arrays.cpp
+++ problem_basic/array/09-find-intersection-of-two-sorted-arrays.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 // Problem: Write a program to find intersection of two sorted arrays in cpp.
 
+void print(vector<int> v){
+    for(int a:v){
+        cout<<a<<" ";
+    }
+    cout<<endl;
+}
+vector<int> findIntersection(vector<int> a,vector<int> b){
+
+    unordered_map<int, int> ump;
+    vector<int> ans;
+
+    if(a.empty() || b.empty()){
+        return ans;
+    }
+
+    for(int i:a){
+        ump.insert({i,i});
+    }
+
+    for(int it:b){
+        if(ump.count(it)){
+            ans.push_back(it);
+            ump.erase(it);
+        }
+    }
+
+    return ans;
+}
+
 int main() {
+
+    vector<int> v={3,4,2,5,256,3,6,2,654,5,7,3,4};
+    vector<int> v2={3,4,1,6,7,2,5,7,2,4,66,3};
+
+    vector<int> result = findIntersection(v, v2);
+
+
+    print(result);
+
     return 0;
 }

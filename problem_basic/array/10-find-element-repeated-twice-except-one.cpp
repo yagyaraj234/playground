3a235ea553a01e583a27be3f1dff42509d857926
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-04-08 10:33:16 +0530

    solved: find unique element

--- problem_basic/array/10-find-element-repeated-twice-except-one.cpp
+++ problem_basic/array/10-find-element-repeated-twice-except-one.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-// Problem: There is an array with every element repeated twice except one. Find that element.
+// Problem:
+// There is an array with every element repeated twice except one.
+// Find that element.
+//
+int findUniqueEl(vector<int> v){
+    unordered_map<int, int> ump;
+
+    for(int a:v){
+        ump[a]++;
+    }
+
+    for(auto it:ump){
+        if(it.second ==1){
+            return it.first;
+        }
+    }
+    return 0;
+}
 
 int main() {
+    vector<int> v={1,2,3,4,5,6,7,7,2,1,6,5,3};
+
+    int result = findUniqueEl(v);
+    cout<<result;
     return 0;
 }
